c316b20529cb7d97a625affba51537487d12a660
William Puello Elles <william.puello@gmail.com>
2026-03-05 17:52:19 -0500

    Proyecto Inicial 4

--- src/main/java/com/yuutoap/Appoiments/config/SecurityConfig.java
+++ src/main/java/com/yuutoap/Appoiments/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("api/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session ->
